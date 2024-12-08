e55ab7c5886c3c5358412804b89a596723ee200b
Alvaro Torres <alvaro@alvarotorres.dev>
2024-12-07 23:43:28 -0600

    Learned scaling, rotating, grouping and more

--- src/script.js
+++ src/script.js
@@ -6,11 +6,38 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Objects
+// Groups
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 3
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+)
+cube2.position.x = -2
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+cube3.position.x = 2
+group.add(cube3);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 // Camera
 const sizes = {
@@ -19,7 +46,9 @@ const sizes = {
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+// camera.position.set(1, 1, 3);
 scene.add(camera);
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
